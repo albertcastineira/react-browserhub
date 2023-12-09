eafb5749b863cdd118beac912e429bb75f7ac079
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2023-12-09 13:23:42 +0100

    Change Categories & tabs

--- src/components/EntertainmentTab.jsx
+++ src/components/EntertainmentTab.jsx
@@ -6,6 +6,7 @@ export default function EntertainmentTab() {
         <WebsiteCard websiteName={"Instagram"} icon={"mdi:instagram"} link={"https://www.instagram.com/"}/>
         <WebsiteCard websiteName={"Twitter"} icon={"pajamas:twitter"} link={"https://twitter.com/"}/>
         <WebsiteCard websiteName={"Youtube"} icon={"mdi:youtube"} link={"https://www.youtube.com/"}/>
+        <WebsiteCard websiteName={"Twitch"} icon={"mdi:twitch"} link={"https://www.twitch.tv/"}/>
         <WebsiteCard websiteName={"Netflix"} icon={"mdi:netflix"} link={"https://www.netflix.com"}/>
         <WebsiteCard websiteName={"Prime Video"} icon={"simple-icons:prime"} link={"https://www.primevideo.com"}/>
         <WebsiteCard websiteName={"Animefenix"} icon={"ph:video-light"} link={"https://animefenix.tv"}/>
--- src/components/SocialMediaTab.jsx
+++ src/components/SocialMediaTab.jsx
@@ -5,7 +5,7 @@ export default function SocialMediaTab() {
     <>
         <WebsiteCard websiteName={"Instagram"} icon={"mdi:instagram"} link={"https://www.instagram.com/"}/>
         <WebsiteCard websiteName={"Twitter"} icon={"pajamas:twitter"} link={"https://twitter.com/"}/>
-        <WebsiteCard websiteName={"Youtube"} icon={"mdi:youtube"} link={"https://www.youtube.com/"}/>
+        <WebsiteCard websiteName={"Facebook"} icon={"mdi:facebook"} link={"https://www.facebook.com/"}/>
         <WebsiteCard websiteName={"Whatsapp"} icon={"ic:baseline-whatsapp"} link={"https://www.whatsapp.com/"}/>
         <WebsiteCard websiteName={"TikTok"} icon={"ic:baseline-tiktok"} link={"https://www.tiktok.com/"}/>
         <WebsiteCard websiteName={"Pinterest"} icon={"mdi:pinterest"} link={"https://www.pinterest.com/ideas/"}/>
