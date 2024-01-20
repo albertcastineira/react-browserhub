787a7376bb5b4bf26552ef562b540fe48092d9a5
Albert Castiñeira Aranda <albertcastiaran@gmail.com>
2024-01-20 02:32:05 +0100

    added coding platforms

--- src/components/JobsTab.jsx
+++ src/components/JobsTab.jsx
@@ -5,7 +5,10 @@ export default function JobsTab() {
     <>
         <WebsiteCard websiteName={"LinkedIn"} icon={"mdi:linkedin"} link={"https://www.linkedin.com/feed/"}/>
         <WebsiteCard websiteName={"InfoJobs"} icon={"arcticons:jobstreet"} link={"https://www.infojobs.net/"}/>
-        <WebsiteCard websiteName={"Arc"} icon={"iconoir:laptop-dev-mode"} link={"https://arc.dev/"}/>
+        <WebsiteCard websiteName={"Arc"} icon={"tabler:parentheses"} link={"https://arc.dev/"}/>
+        <WebsiteCard websiteName={"Circular"} icon={"entypo:circular-graph"} link={"https://circular.io/"}/>
+        <WebsiteCard websiteName={"LeetCode"} icon={"simple-icons:leetcode"} link={"https://leetcode.com/"}/>
+        <WebsiteCard websiteName={"Codewars"} icon={"cib:codewars"} link={"https://www.codewars.com/dashboard"}/>
     </>
   )
 }
